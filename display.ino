7498413321652d862c9917136dfc697348cfc990
Irenee <icaroulle@gmail.com>
2022-04-03 17:29:48 +0200

    Use SPI

--- display.ino
+++ display.ino
@@ -1,79 +1,95 @@
-#define RST 12
-#define CE 11
-#define DC 10
-#define DIN 9
-#define CLK 8
+#include "SPI.h"
 
-void lcd_write_cmd(byte cmd)
-{
-  digitalWrite(DC, LOW); // DC pin is low for commands
-  digitalWrite(CE, LOW);
-  shiftOut(DIN, CLK, MSBFIRST, cmd); // transmit serial data
-  digitalWrite(CE, HIGH);
-}
+#define RST 12
+#define DC 10
 
 void setup()
 {
-  pinMode(RST, OUTPUT);
-  pinMode(CE, OUTPUT);
   pinMode(DC, OUTPUT);
-  pinMode(DIN, OUTPUT);
-  pinMode(CLK, OUTPUT);
+  pinMode(RST, OUTPUT);
+
+  SPI.begin(); // set SCK, MOSI, and SS to outputs, pulling SCK and MOSI low, and SS high
 
   digitalWrite(RST, LOW);
   digitalWrite(RST, HIGH);
 
-  lcd_write_cmd(0x21); // LCD extended commands
-  lcd_write_cmd(0xB8); // set LCD Vop (contrast)
-  lcd_write_cmd(0x04); // set temp coefficient
-  lcd_write_cmd(0x14); // LCD bias mode 1:40
-  lcd_write_cmd(0x20); // LCD basic commands
+  digitalWrite(SS, LOW); // Activate
+  digitalWrite(DC, LOW); // DC pin is low for commands
+  SPI.beginTransaction(SPISettings(16000000, MSBFIRST, SPI_MODE0));
+  SPI.transfer(0x21); // LCD extended commands
+  SPI.transfer(0xB8); // set LCD Vop (contrast)
+  SPI.transfer(0x04); // set temp coefficient
+  SPI.transfer(0x14); // LCD bias mode 1:40
+  SPI.endTransaction();
+  digitalWrite(SS, HIGH); // Deactivate
 
   // Clear display data ram
-  digitalWrite(CE, LOW);
-  digitalWrite(DC, LOW);              // DC pin is low for commands
-  shiftOut(DIN, CLK, MSBFIRST, 0x0C); // normal display mode
-  shiftOut(DIN, CLK, MSBFIRST, 0x40); // y 0
-  shiftOut(DIN, CLK, MSBFIRST, 0x80); // x 0
-  digitalWrite(DC, HIGH);             // DC pin is high for data
+  digitalWrite(SS, LOW); // Activate
+
+  digitalWrite(DC, LOW); // DC pin is low for commands
+  SPI.beginTransaction(SPISettings(16000000, MSBFIRST, SPI_MODE0));
+  SPI.transfer(0x20); // LCD basic commands
+  SPI.transfer(0x0C); // normal display mode
+  SPI.transfer(0x40); // y 0
+  SPI.transfer(0x80); // x 0
+  SPI.endTransaction();
+
+  digitalWrite(DC, HIGH); // DC pin is high for data
+  SPI.beginTransaction(SPISettings(16000000, MSBFIRST, SPI_MODE0));
   for (int i = 0; i < 48 * 84; i++)
   {
-    shiftOut(DIN, CLK, MSBFIRST, 0x00);
+    SPI.transfer(0x00);
   }
-  digitalWrite(DC, LOW); // DC pin is low for commands
-  digitalWrite(CE, HIGH);
+  SPI.endTransaction();
+
+  digitalWrite(SS, HIGH); // Deactivate
 }
 
 void loop()
 {
   // LCD all segments on
   delay(1000);
-  lcd_write_cmd(0x09);
+  digitalWrite(SS, LOW); // Activate
+  digitalWrite(DC, LOW); // DC pin is low for commands
+  SPI.beginTransaction(SPISettings(16000000, MSBFIRST, SPI_MODE0));
+  SPI.transfer(0x20); // LCD basic commands
+  SPI.transfer(0x09); // On
+  SPI.endTransaction();
+  digitalWrite(SS, HIGH); // Deactivate
 
   // LCD all segments off
   delay(1000);
-  lcd_write_cmd(0x08);
+  digitalWrite(SS, LOW); // Activate
+  digitalWrite(DC, LOW); // DC pin is low for commands
+  SPI.beginTransaction(SPISettings(16000000, MSBFIRST, SPI_MODE0));
+  SPI.transfer(0x20); // LCD basic commands
+  SPI.transfer(0x08); // Off
+  SPI.endTransaction();
+  digitalWrite(SS, HIGH); // Deactivate
 
   // Draw 8x8 sphere
   delay(1000);
-  digitalWrite(CE, LOW); // Activate
+  digitalWrite(SS, LOW); // Activate
 
-  digitalWrite(DC, LOW);              // DC pin is low for commands
-  shiftOut(DIN, CLK, MSBFIRST, 0x20); // basic instruction mode
-  shiftOut(DIN, CLK, MSBFIRST, 0x0C); // normal display mode
-  shiftOut(DIN, CLK, MSBFIRST, 0x40); // y 0
-  shiftOut(DIN, CLK, MSBFIRST, 0x80); // x 0
+  digitalWrite(DC, LOW); // DC pin is low for commands
+  SPI.beginTransaction(SPISettings(16000000, MSBFIRST, SPI_MODE0));
+  SPI.transfer(0x20); // LCD basic commands
+  SPI.transfer(0x0C); // normal display mode
+  SPI.transfer(0x40); // y 0
+  SPI.transfer(0x80); // x 0
+  SPI.endTransaction();
 
-  digitalWrite(DC, HIGH);             // DC pin is high for data
-  shiftOut(DIN, CLK, MSBFIRST, 0x3C); // sprite data..
-  shiftOut(DIN, CLK, MSBFIRST, 0x7E);
-  shiftOut(DIN, CLK, MSBFIRST, 0xFF);
-  shiftOut(DIN, CLK, MSBFIRST, 0xFF);
-  shiftOut(DIN, CLK, MSBFIRST, 0xFF);
-  shiftOut(DIN, CLK, MSBFIRST, 0xFF);
-  shiftOut(DIN, CLK, MSBFIRST, 0x7E);
-  shiftOut(DIN, CLK, MSBFIRST, 0x3C);
+  digitalWrite(DC, HIGH); // DC pin is high for data
+  SPI.beginTransaction(SPISettings(16000000, MSBFIRST, SPI_MODE0));
+  SPI.transfer(0x3C); // sprite data..
+  SPI.transfer(0x7E);
+  SPI.transfer(0xFF);
+  SPI.transfer(0xFF);
+  SPI.transfer(0xFF);
+  SPI.transfer(0xFF);
+  SPI.transfer(0x7E);
+  SPI.transfer(0x3C);
+  SPI.endTransaction();
 
-  digitalWrite(DC, LOW);  // DC pin is low for commands
-  digitalWrite(CE, HIGH); // Deactivate
+  digitalWrite(SS, HIGH); // Deactivate
 }
