0dddccd290b443b962b15acdacc5c160d8409bce
Irenee <icaroulle@gmail.com>
2022-04-15 20:32:25 +0200

    Ball collisions

--- display.ino
+++ display.ino
@@ -2,49 +2,83 @@
 
 uint8_t gfx_ball[8] = {0x3C, 0x7A, 0xFD, 0xFF, 0xFF, 0xFF, 0x7E, 0x3C};
 
+// speeds for 60fps 40px/s at 3pi/8, 2pi/8, pi/8
+int16_t speed_x[4] = {65, 121, 158, 0};
+int16_t speed_y[4] = {158, 121, 65, 0};
+
 class Ball
 {
 private:
-  int8_t x;
-  int8_t y;
+  int16_t x;
+  int16_t y;
   PCD8544::sprite_t sprite;
 
 public:
   Ball()
   {
-    x = (PCD8544::SCREEN_WIDTH - 8) / 2;
-    y = (PCD8544::SCREEN_HEIGHT - 8) / 2;
-
-    sprite = {0x01, uint8_t(x), uint8_t(y), gfx_ball};
   }
 
-  void move(int8_t dx, int8_t dy)
+  void reset()
   {
-    x += dx;
-    y += dy;
+    x = ((PCD8544::SCREEN_WIDTH - 8) / 2) << 8;
+    y = ((PCD8544::SCREEN_HEIGHT - 8) / 2) << 8;
 
+    sprite = {0x01, uint8_t(x), uint8_t(y), gfx_ball};
+
+    sprite.x = x >> 8;
+    sprite.y = y >> 8;
+
+    PCD8544::set_sprite(0, sprite);
+  }
+
+  uint8_t update(uint8_t speed_id)
+  {
+    uint8_t collision = false;
+
+    int16_t sx = speed_x[speed_id];
+    int16_t sy = speed_y[speed_id];
+
+    x += sx;
+    y += sy;
+
+    // Handle collisions.
+    // max_x & max_y are slightly out of bound but give good results on screen.
     if (x < 0)
     {
-      x = 0;
+      x = -x;
+      speed_x[speed_id] = -speed_x[speed_id];
+      collision = 1;
     }
-    if (x > PCD8544::SCREEN_WIDTH - 8)
+
+    int16_t max_x = ((PCD8544::SCREEN_WIDTH - 8) << 8) | 0xFF;
+    if (x > max_x)
     {
-      x = PCD8544::SCREEN_WIDTH - 8;
+      x = -x + (2 * max_x);
+      speed_x[speed_id] = -speed_x[speed_id];
+      collision = 1;
     }
 
     if (y < 0)
     {
-      y = 0;
-    }
-    if (y > PCD8544::SCREEN_HEIGHT - 8)
-    {
-      y = PCD8544::SCREEN_HEIGHT - 8;
+      y = -y;
+      speed_y[speed_id] = -speed_y[speed_id];
+      collision = 1;
     }
 
-    sprite.x = x;
-    sprite.y = y;
+    int16_t max_y = ((PCD8544::SCREEN_HEIGHT - 8) << 8) | 0xFF;
+    if (y > max_y)
+    {
+      y = -y + (2 * max_y);
+      speed_y[speed_id] = -speed_y[speed_id];
+      collision = 1;
+    }
+
+    sprite.x = x >> 8;
+    sprite.y = y >> 8;
 
     PCD8544::set_sprite(0, sprite);
+
+    return collision;
   }
 };
 
@@ -102,9 +136,7 @@ Paddle right(0x0);
 void setup()
 {
   PCD8544::setup();
-  ball.move(0, 0);
-  left.move(0);
-  right.move(0);
+  ball.reset();
 }
 
 uint8_t tick = 10;
@@ -112,9 +144,7 @@ int8_t deltas[2] = {1, -1};
 
 void loop()
 {
-  ball.move(deltas[0], deltas[1]);
-  left.move(deltas[0]);
-  right.move(deltas[1]);
+  uint8_t collision = ball.update(2);
 
   tick++;
   if (tick > 20)
@@ -125,5 +155,5 @@ void loop()
   }
 
   PCD8544::render();
-  delay(100);
+  delay(collision ? 16 : 16);
 }
