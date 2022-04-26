d7ee2b59713d067cafdf73f056688c89c8174858
Irenee <icaroulle@gmail.com>
2022-04-26 21:10:17 +0200

    Add controller input

--- frapball.ino
+++ frapball.ino
@@ -5,12 +5,18 @@
 #include "src/game/paddle.h"
 #include "src/game/ybuffer.h"
 
+const uint8_t PIN_BUTTON_UP = 2;
+const uint8_t PIN_BUTTON_DOWN = 3;
+
 Ball ball;
 Paddle left(0x1);
 Paddle right(0x0);
 
 void setup()
 {
+  pinMode(PIN_BUTTON_UP, INPUT);
+  pinMode(PIN_BUTTON_DOWN, INPUT);
+
   PCD8544::setup();
   ball.reset();
   left.move(0);
@@ -27,7 +33,6 @@ void loop()
   ball.update();
 
   uint16_t col_data = 0;
-
   col_data |= compute_collision((ball.x >> 8) + 4,
                                 (ball.y >> 8) + 4,
                                 4,
@@ -46,15 +51,21 @@ void loop()
 
   ball.post_collision_update(col_data);
 
+  // Player controls the left paddle
+  uint8_t button_up_pressed = digitalRead(PIN_BUTTON_UP) == HIGH;
+  uint8_t button_down_pressed = digitalRead(PIN_BUTTON_DOWN) == HIGH;
+  if (button_up_pressed)
+    left.move(-1);
+  if (button_down_pressed)
+    left.move(+1);
+
+  // The right paddle targets the ball y pos with a few frames of lag.
   y_buffer[y_index] = (ball.y >> 8) + 4;
-
-  uint8_t yl = (y_index + Y_BUFFER_SIZE - 8) % Y_BUFFER_SIZE; // 8 frames of lag
-  uint8_t yr = (y_index + Y_BUFFER_SIZE - 4) % Y_BUFFER_SIZE; // 4 frames of lag
-
-  left.target(y_buffer[yl]);
-  right.target(y_buffer[yr]);
-
+  uint8_t yl = (y_index + Y_BUFFER_SIZE - 8) % Y_BUFFER_SIZE;  // 8 frames of lag
+  uint8_t yr = (y_index + Y_BUFFER_SIZE - 14) % Y_BUFFER_SIZE; // 14 frames of lag
   y_index = (y_index + 1) % Y_BUFFER_SIZE;
+
+  right.target(y_buffer[yr]);
 
   PCD8544::render();
   delay(30);
