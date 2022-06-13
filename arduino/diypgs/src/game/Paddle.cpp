bb19e54369d88495b7189e5ddf7cb8000d6b32b1
Irenee <icaroulle@gmail.com>
2022-06-13 21:36:05 +0200

    Paddle blinking before applying bonus effect

--- arduino/diypgs/src/game/Paddle.cpp
+++ arduino/diypgs/src/game/Paddle.cpp
@@ -12,6 +12,8 @@ namespace game
 
         constexpr uint8_t GFX_SMALL_PADDLE_LEFT[8] = {0x00, 0x00, 0x00, 0x00, 0x00, 0x3C, 0x7E, 0xFF};
         constexpr uint8_t GFX_SMALL_PADDLE_RIGHT[8] = {0xFF, 0x7E, 0x3C, 0x00, 0x00, 0x00, 0x00, 0x00};
+
+        constexpr uint8_t TRANSITION_FRAME_COUNT = 32;
     }
 
     void Paddle::setup(const Side side_)
@@ -24,11 +26,63 @@ namespace game
         size = Size::NORMAL;
         inverted = 0;
 
+        transition = Transition::NONE;
+        transitionFrameCounter = 0;
+
         sprite_ids[0] = (side == Side::LEFT) ? 4 : 5;
         sprite_ids[1] = (side == Side::LEFT) ? 6 : 7;
 
         sprites[0] = {hw::Pcd8544::Sprite::Flag::ENABLED, x, y, 0xFF, (side == Side::LEFT) ? GFX_PADDLE_LEFT_TOP : GFX_PADDLE_RIGHT_TOP};
         sprites[1] = {hw::Pcd8544::Sprite::Flag::ENABLED, x, int8_t(y + 8), 0xFF, (side == Side::LEFT) ? GFX_PADDLE_LEFT_BOTTOM : GFX_PADDLE_RIGHT_BOTTOM};
+
+        hw::Pcd8544::set_sprite(sprite_ids[0], sprites[0]);
+        hw::Pcd8544::set_sprite(sprite_ids[1], sprites[1]);
+    }
+
+    void Paddle::update()
+    {
+        if (transitionFrameCounter > 0)
+        {
+            const uint8_t enabled = (transitionFrameCounter >> 2) & 1;
+            if (enabled)
+            {
+                for (int i = 0; i <= size; i++)
+                {
+                    sprites[i].flags |= hw::Pcd8544::Sprite::Flag::ENABLED;
+                }
+            }
+            else
+            {
+                for (int i = 0; i <= size; i++)
+                {
+                    sprites[i].flags &= ~hw::Pcd8544::Sprite::Flag::ENABLED;
+                }
+            }
+
+            transitionFrameCounter--;
+            if (transitionFrameCounter == 0)
+            {
+                switch (transition)
+                {
+                case Transition::INVERT_DIRECTION:
+                    applyInvertDirection();
+                    break;
+
+                case Transition::CHANGE_SIZE:
+                    applyChangeSize();
+                    break;
+                }
+
+                transition = 0;
+            }
+        }
+        else
+        {
+            for (int i = 0; i <= size; i++)
+            {
+                sprites[i].flags |= hw::Pcd8544::Sprite::Flag::ENABLED;
+            }
+        }
 
         hw::Pcd8544::set_sprite(sprite_ids[0], sprites[0]);
         hw::Pcd8544::set_sprite(sprite_ids[1], sprites[1]);
@@ -63,13 +117,26 @@ namespace game
         }
     }
 
-    void Paddle::invert()
+    // Signal transition then applyInvertDirection after a few frames.
+    void Paddle::invertDirection()
+    {
+        transitionFrameCounter = TRANSITION_FRAME_COUNT;
+        transition = Transition::INVERT_DIRECTION;
+    }
+
+    void Paddle::applyInvertDirection()
     {
         inverted = !inverted;
     }
 
-    // Decide the new size based on the paddle y pos.
+    // Signal transition then applyChangeSize after a few frames.
     void Paddle::changeSize()
+    {
+        transitionFrameCounter = TRANSITION_FRAME_COUNT;
+        transition = Transition::CHANGE_SIZE;
+    }
+
+    void Paddle::applyChangeSize()
     {
         if (size == Size::NORMAL)
         {
